0fe768b4cf176ffc0743a0f90e36b1b6a20ccf76
vanvi <kania.praca@wp.pl>
2024-05-29 20:22:54 +0200

    Create bean for Bcrypt

--- src/main/java/io/christianj98/securecapita/SecurecapitaApplication.java
+++ src/main/java/io/christianj98/securecapita/SecurecapitaApplication.java
@@ -2,12 +2,20 @@ package io.christianj98.securecapita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SecurecapitaApplication {
+
+	private static final int STRENGTH = 12;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurecapitaApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder(STRENGTH);
+	}
 }
